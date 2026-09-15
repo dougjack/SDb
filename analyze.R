f1d5decbd08fa4755ec1e9fafbf86657ee19dc29
Doug Jackson <doug@QEDAconsulting.com>
2026-09-15 16:27:51 -0700

    Analyze neutrally buoyant and surface-oriented particles.

--- analyze.R
+++ analyze.R
@@ -73,7 +73,7 @@ analyzeSalmon <- function(dF, loc) {
         p <- ggplot(thisDFwide) + geom_point(aes(x=baseline, y=preferred, group=month, color=month), alpha=0.25) +
             geom_abline(intercept=0, slope=1, color="red") +
             facet_wrap(~month, ncol=1) +
-            labs(title=paste(var, "comparison,", loc), x="survival, baseline scenario", y=paste0(varType, ", preferred alternative")) +
+            labs(title=paste(var, "comparison,", loc), x=paste0(varType, ", baseline scenario"), y=paste0(varType, ", preferred alternative")) +
             theme_light()
         ggsave(file.path(thisOutputDir, paste0("compare_", loc, "_", var, ".png")), width=figWidth, height=figHeight)
         
@@ -102,6 +102,101 @@ analyzeSalmon <- function(dF, loc) {
         cat(paste0(var, ", ", loc, "\n"))
         print(sigs)
         
+    }
+    
+    return(list(dF=dF))
+}
+
+readParticles <- function(files) {
+    datList <- list()
+    for(f in files) {
+        thisScen <- gsub(".dat", "", basename(f))
+        if(thisScen=="np_90day_D-GO-sdg9b") {
+            thisDat <- read.table(f, skip=1, sep="\t", header=T)
+        } else {
+            thisDat <- read.csv(f, skip=1, header=T)
+        }
+        
+        thisDat$scenario <- thisScen
+        thisDat$startDate <- dmy(thisDat$SimPeriod)
+        
+        datList[[length(datList)+1]] <- thisDat
+    }
+    dat <- bind_rows(datList)
+}
+
+analyzeParticles <- function(dF, type) {
+    
+    yLab <- "90-day flux"
+    
+    dF <- dF |> mutate(scenario=ifelse(scenario %in% c("np_90day_D-GO-BSL-10yr-b", "sp_90day_D-GO-BSL-10yr-b"), "baseline", "preferred")) |> 
+        select(startDate, SimLoc:scenario)
+    
+    vars <- names(dF)[which(!(names(dF) %in% c("startDate", "SimLoc", "scenario")))]
+    
+    locs <- unique(dF$SimLoc)
+    
+    for(var in vars) {
+        
+        thisOutputDir <- file.path(outputDir, type, var)
+        dir.create(thisOutputDir, recursive=T, showWarnings=F)
+        
+        for(releaseLoc in locs) {
+            thisDF <- dF |> filter(SimLoc==releaseLoc)
+            thisDF <- thisDF[, c("startDate", "scenario", var)]
+            names(thisDF) <- c("startDate", "scenario", "var")
+            
+            if(sum(!is.na(thisDF$var))==0 | sum(thisDF$var!=0)==0) {next}
+            
+            thisDFwide <- thisDF |> pivot_wider(id_cols=startDate, names_from=scenario, values_from=var) |> 
+                mutate(year=as.factor(year(startDate)), month=as.factor(month(startDate)), julianDay=yday(startDate),
+                       diff=preferred-baseline)
+            
+            p <- ggplot(thisDF) + geom_point(aes(x=startDate, y=var)) +
+                facet_wrap(~scenario, ncol=1) +
+                labs(title=paste0(var, ", release node ", releaseLoc), x="", y=yLab) +
+                theme_light()
+            ggsave(file.path(thisOutputDir, paste0("timeSeries_", releaseLoc, "_", var, ".png")), width=figWidth, height=figHeight)
+            
+            p <- ggplot(thisDFwide) + geom_point(aes(x=year, y=diff, color=month), size=2) +
+                facet_wrap(~month, ncol=1, scales="free_y") +
+                labs(title=paste0("Difference in ", var, ", release node ", releaseLoc), x="Julian day", y=paste("90-day flux difference (preferred - baseline)")) +
+                guides(color=guide_legend(override.aes=list(size=2))) +
+                theme_light()
+            ggsave(file.path(thisOutputDir, paste0("diff_", releaseLoc, "_", var, ".png")), width=figWidth, height=figHeight)
+            
+            p <- ggplot(thisDFwide) + geom_point(aes(x=baseline, y=preferred, group=month, color=month), size=2, alpha=1) +
+                geom_abline(intercept=0, slope=1, color="red") +
+                facet_wrap(~month, ncol=1) +
+                labs(title=paste(var, "comparison, release node", releaseLoc), x="90-day flux, baseline scenario", y=paste0("90-day flux, preferred alternative")) +
+                theme_light()
+            ggsave(file.path(thisOutputDir, paste0("compare_", releaseLoc, "_", var, ".png")), width=figWidth, height=figHeight)
+
+            ####################################################################################################
+            # Boxplots with significance
+            # From Wikipedia: "The Wilcoxon test is a good alternative to the t-test when the normal distribution of the 
+            # differences between paired individuals cannot be assumed. Instead, it assumes a weaker hypothesis that the 
+            # distribution of this difference is symmetric around a central value and it aims to test whether this center 
+            # value differs significantly from zero."
+            sigs <- thisDFwide |> group_by(month) |> filter(n_distinct(diff)>1) |> 
+                summarize(pValResampleMean=resampleDiff(diff),
+                          pValResampleMed=resampleMedDiff(diff),
+                          pValPairedT=t.test(preferred, baseline, paired=T, alternative="two.sided")$p.value,
+                          pValShapiro=shapiro.test(diff)$p.value,
+                          pWilcox = wilcox.test(baseline, preferred, paired=T)$p.value)
+            
+            sigMonths <- sigs |> filter(pWilcox<0.05)
+            sigMonths <- sigMonths$month
+            
+            p <- ggplot(thisDFwide) + geom_boxplot(aes(x=month, y=diff)) + 
+                annotate("text", x=sigMonths, y=max(thisDFwide$diff)*1.05, label="*", size=8, color="blue") +
+                labs(title=paste(var, "comparison, release node", releaseLoc), x="month", y=paste("90-day flux difference (preferred - baseline)")) +
+                theme_light()
+            ggsave(file.path(thisOutputDir, paste0("boxPlot_", releaseLoc, "_", var, ".png")), width=figWidth, height=figHeight)
+            cat("--------------------------------------------------------------------\n")
+            cat(paste0(var, ", ", releaseLoc, "\n"))
+            print(sigs)
+        }
     }
     
     return(list(dF=dF))
@@ -144,13 +239,24 @@ setwd(workingDir)
 outputDir <- file.path(workingDir, "output_analyze")
 dir.create(outputDir, showWarnings=F)
 
+####################################################################################################
+# salmon
 northDeltaFiles <- list.files(dataDir, pattern="salmon_nd*", full.names=T)
 southDeltaFiles <- list.files(dataDir, pattern="salmon_sd*", full.names=T)
-npFiles <- list.files(dataDir, pattern="np_*", full.names=T)
-spFiles <- list.files(dataDir, pattern="sp_*", full.names=T)
 
 northDelta <- readSalmon(northDeltaFiles)
 out <- analyzeSalmon(northDelta, "northDelta")
 
 southDelta <- readSalmon(southDeltaFiles)
 out <- analyzeSalmon(southDelta, "southDelta")
+
+####################################################################################################
+# neutrally buoyant and surface-oriented particles
+npFiles <- list.files(dataDir, pattern="np_*", full.names=T)
+spFiles <- list.files(dataDir, pattern="sp_*", full.names=T)
+
+np <- readParticles(npFiles)
+out <- analyzeParticles(np, "neutrallyBuoyant")
+
+sp <- readParticles(spFiles)
+out <- analyzeParticles(sp, "surfaceOriented")
